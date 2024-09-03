62e28354d22cce150391ba96bc3b50675262a326
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-09-03 01:25:09 -0500

    Remove unneeded directories/files

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const app = express();
 const PORT = 3000;
-const newRouter = require("./routes/newRouter");
-const indexRouter = require("./routes/indexRouter");
 const path = require("node:path");
-
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/new", newRouter);
-app.use("/", indexRouter);
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+];
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index", { messages: messages });
+});
+
+app.get("/new", (req, res) => {
+    res.render("form");
+});
+
+app.post("/new", (req, res) => {
+    console.log(req.body)
+    const author = req.body.author;
+    const message = req.body.message;
+    console.log(`Author: ${author}`);
+    console.log(`Message: ${message}`);
+    messages.push({ text: message, user: author, added: new Date() });
+    res.redirect("/")
+})
 
 
 app.listen(PORT, () => {console.log(`Express Mini Messaging Board - Listening on Port ${PORT}`)})
