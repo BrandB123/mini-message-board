6bb31300713eea8bfe964959a1af494b71fffea4
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-09-03 00:09:09 -0500

    Add home page content with prefilled messages

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const PORT = 3000;
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
